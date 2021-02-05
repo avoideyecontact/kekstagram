ed4d58e66c4b21d2597c526143c4b3062533904e
avoideyecontact <vendetta_gt@mail.ru>
2021-02-05 18:27:38 +0500

    Внёс правки

--- js/main.js
+++ js/main.js
@@ -1,19 +1,13 @@
 // Проверки максимальной длины строки
-function checkLength(yourString, maxLength) {
+function checkStrLength(yourString, maxLength) {
   return yourString.length <= maxLength;
 }
 
 // Целое число из диапазона "min...max"
 function getRandomInt(min, max) {
-  if (max > min) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-  else if (min == max) {
-    return min;
-  }
-  else return Math.floor(Math.random() * (min - max + 1)) + max;
+  return Math.abs(Math.floor(Math.random() * (min - max + 1)) + max);
 }
 
-checkLength('some string', 16);
+checkStrLength('some string', 16);
 
 getRandomInt(1, 9);
