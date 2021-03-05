bd14173c85f67b78277eba19d785275f83b72551
avoideyecontact <vendetta_gt@mail.ru>
2021-03-05 20:41:46 +0500

    Выполнил разбиение на модули

---
+++ js/util.js
@@ -0,0 +1,19 @@
+// Проверки максимальной длины строки
+function checkStrLength(yourString, maxLength) {
+  return yourString.length <= maxLength;
+}
+
+// Целое число из диапазона "min...max"
+function getRandomInt(min, max) {
+  return Math.abs(Math.floor(Math.random() * (min - max + 1)) + max);
+}
+
+checkStrLength('some string', 16);
+getRandomInt(1, 9);
+
+// Возвращает случайный элемент массива
+function getRandomArrayElement(array) {
+  return array[getRandomInt(0, array.length - 1)];
+}
+
+export { getRandomInt, getRandomArrayElement };
